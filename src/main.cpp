e87bfdf768d2af6d428c86d872fe42f75a72bd79
García Barreiro <garcia.barreiro@proton.me>
2025-03-24 12:45:34 +0100

    feat: rotation, working

--- src/main.cpp
+++ src/main.cpp
@@ -26,48 +26,13 @@ arma::mat cov(const std::vector<Lpoint>& points)
 {
 	auto c = centroid(points);
 
-	double x_std = 0, y_std = 0, z_std = 0;
-	for (const Lpoint& p : points) {
-		x_std += pow(p.getX() - c.getX(), 2);
-		y_std += pow(p.getY() - c.getY(), 2);
-		z_std += pow(p.getZ() - c.getZ(), 2);
-	}
-	x_std = sqrt(x_std/points.size());
-	y_std = sqrt(y_std/points.size());
-	z_std = sqrt(z_std/points.size());
-
     arma::mat cov(3, 3, arma::fill::zeros);
     for (const Lpoint& p : points)
     {
-        arma::vec v = { (p.getX() - c.getX())/x_std, (p.getY() - c.getY())/y_std, (p.getZ() - c.getZ())/z_std };
+        arma::vec v = { p.getX() - c.getX(), p.getY() - c.getY(), p.getZ() - c.getZ() };
         cov += v * v.t();
     }
     return cov / points.size();
-}
-
-// TODO: clean
-arma::mat rot(const arma::vec& a, float angle)
-{
-	arma::mat R(3,3);
-
-	auto cosa = cos(angle);
-	auto mcosa = 1 - cosa;
-	auto sina = sin(angle);
-
-	// (row, column)
-	R(0,0) = a(0)*a(0)*mcosa + cosa;
-	R(0,1) = a(0)*a(1)*mcosa - a(2)*sina;
-	R(0,2) = a(0)*a(2)*mcosa + a(1)*sina;
-
-	R(1,0) = a(1)*a(0)*mcosa + a(2)*sina;
-	R(1,1) = a(1)*a(1)*mcosa + cosa;
-	R(1,2) = a(1)*a(2)*mcosa - a(0)*sina;
-
-	R(2,0) = a(2)*a(0)*mcosa - a(1)*sina;
-	R(2,0) = a(2)*a(1)*mcosa + a(0)*sina;
-	R(2,0) = a(2)*a(2)*mcosa + cosa;
-
-	return R;
 }
 
 int main(int argc, char* argv[])
@@ -108,36 +73,29 @@ int main(int argc, char* argv[])
 	arma::mat eigvec;
 	arma::eig_sym(eigval, eigvec, A);
 
-	// find max eigval and corresponding eigvec
+	// find max eigval and corresponding eigvec (TODO: clean)
 	float max = eigval[0];
 	int idx = 0;
 	for (int i = 1; i < 3; i++) {
-		if (eigval[i] > max) {
+		if (abs(eigval[i]) > max) {
 			idx = i;
 		}
 	}
 
-	// rotate alongside (1,0,0), (0,1,0) or (0,0,1)
-	// TODO: clean
-	arma::vec axis {1,0,0};	// axis
-	if (idx == 1) axis = {0,1,0};
-	else if (idx == 2) axis = {0,0,1};
+	// normalise eigenvector
+	arma::vec unit = arma::normalise(eigvec.col(idx));
+
+	// rotate unitary eigenvec to (1,0,0)
+	arma::vec axis {1,0,0};	// column vector
 
 	// cmath acos (arma::acos returns error)
 	auto angle = acos(arma::norm_dot(eigvec.col(idx), axis));
 
-	// calc rotation matrix (TODO: cleaner way?)
-	auto R = rot(axis, angle);
-
-	std::cout << A << "\n";
-	
-	std::cout << eigval << "\n";
-	std::cout << eigvec << "\n";
-
-	std::cout << R << "\n";
+	// calc rotation matrix
+	arma::mat33 R = arma::eye(3,3) - ((unit + axis)/(1 + arma::dot(unit, axis)))
+					* arma::trans(unit + axis) + 2 * axis * arma::trans(unit);
 
 	// apply rotation to all points in point cloud and write point cloud (txt for now)
-	// standardize points????
 	for (const Lpoint& p : points) {
 		arma::mat point(3,1);
 		point(0) = p.getX();
